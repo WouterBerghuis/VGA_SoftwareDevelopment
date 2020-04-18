8d81305192b8c663899e95d2d68bbc516d3f58bb
bestshield <33286246+bestshield@users.noreply.github.com>
2020-04-19 00:23:29 +0200

    Update main.c

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -149,7 +149,7 @@ void SystemClock_Config(void)
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
-
+  printf(ok);
   if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
   {
     Error_Handler();
