ecc6a9c6949b6de0d974f10a32e5378b03c0978d
WouterBerghuis <berghuis@delmic.com>
2020-04-22 14:33:53 +0200

    Added angry smiley bitmap

---
+++ Library/bitmaps/smiley_angry.h
@@ -0,0 +1,23 @@
+/**
+  ******************************************************************************
+  * @file	  			smiley_angry.h
+  *
+  * @author 			Wouter Berghuis
+  * @date 				22 April 2020
+  * @brief        		This file contains all the needed defines for the
+  * 					'smiley_angry' bitmap.
+  ******************************************************************************
+**/
+
+#include "stdint.h"
+
+#ifndef __SMILEY_ANGRY_H
+#define __SMILEY_ANGRY_H
+
+#define SMILEY_ANGRY_WIDTH 				64
+#define SMILEY_ANGRY_HEIGHT 			64
+#define BITMAP_NUMBER_SMILEY_ANGRY 		1
+
+extern uint8_t smiley_angry[SMILEY_ANGRY_WIDTH * SMILEY_ANGRY_HEIGHT];
+
+#endif //__SMILEY_ANGRY_H
