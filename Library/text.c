1e3d7f0a63aa700c55d49d3484c18476d03b8f90
WouterBerghuis <berghuis@delmic.com>
2020-04-27 11:24:21 +0200

    added functionality to choose two fontsizes

--- Library/text.c
+++ Library/text.c
@@ -4,6 +4,7 @@
 TEXT_ERROR_CODES API_Draw_Text(uint16_t x_coor, uint16_t y_coor, uint8_t color, char *text, char *fontname,
 					 	 	 	uint8_t fontsize, uint8_t fontstyle){
 
+	TEXT_ERROR_CODES error;
 	uint16_t x_coor_cursor = x_coor;
 	uint16_t y_coor_cursor = y_coor;
 	size_t text_length = strlen(text);
@@ -12,33 +13,92 @@ TEXT_ERROR_CODES API_Draw_Text(uint16_t x_coor, uint16_t y_coor, uint8_t color, 
 	if(strcmp(fontname, "Minecraft") != 0)
 		return ERROR_FONT_NOT_AVAILABLE;
 
-	if((x_coor + (text_length * MAX_CHARACTER_WIDTH) ) > VGA_DISPLAY_X)
-		return ERROR_TEXT_TOO_LONG;
+	switch(fontsize){
+	case (FONTSIZE_SMALL):
+		if((x_coor + (text_length * MAX_SMALL_CHARACTER_WIDTH)) > VGA_DISPLAY_X)
+			return ERROR_TEXT_TOO_LONG;
 
-	for(uint32_t i = 0; i < text_length; i++){
-		uint8_t characterNumber  = *text;
-		characterNumber 		-= ASCII_OFFSET;
-		Pbitmap 				 = &Minecraft_Font[characterNumber][0];
+		for(uint32_t i = 0; i < text_length; i++){
+			uint8_t characterNumber  = *text;
+			characterNumber 		-= ASCII_OFFSET;
+			Pbitmap 				 = &Minecraft_Font[characterNumber][0];
 
-		API_Write_Character_to_VGA(x_coor_cursor, y_coor_cursor, MINECRAFT_CHARACTERS_WIDTH, MINECRAFT_CHARACTERS_HEIGHT, color, Pbitmap);
+			error = API_Write_Small_Character_to_VGA(x_coor_cursor, y_coor_cursor, MINECRAFT_CHARACTERS_WIDTH, MINECRAFT_CHARACTERS_HEIGHT, color, Pbitmap);
+			if(error == ERROR_WRITE_CHARACTER)
+				return error;
 
-		x_coor_cursor += MINECRAFT_CHARACTERS_WIDTH;
-		text++;
+			x_coor_cursor += MINECRAFT_CHARACTERS_WIDTH;
+			text++;
+		}
+
+		return DRAW_TEXT_SUCCESS;
+
+	case(FONTSIZE_BIG):
+		if((x_coor + (text_length * MAX_BIG_CHARACTER_WIDTH)) > VGA_DISPLAY_X)
+			return ERROR_TEXT_TOO_LONG;
+
+		for(uint32_t i = 0; i < text_length; i++){
+			uint8_t characterNumber  = *text;
+			characterNumber 		-= ASCII_OFFSET;
+			Pbitmap 				 = &Minecraft_Font[characterNumber][0];
+
+			error = API_Write_Big_Character_to_VGA(x_coor_cursor, y_coor_cursor, (MINECRAFT_CHARACTERS_WIDTH * 2), (MINECRAFT_CHARACTERS_HEIGHT * 2), color, Pbitmap);
+			if(error == ERROR_WRITE_CHARACTER)
+				return error;
+
+			x_coor_cursor += (MINECRAFT_CHARACTERS_WIDTH * 2);
+			text++;
+		}
+
+		return DRAW_TEXT_SUCCESS;
+
+	default:
+		return ERROR_FONTSIZE_NOT_AVAILABLE;
 	}
-
-	return DRAW_TEXT_SUCCESS;
 }
 
-TEXT_ERROR_CODES API_Write_Character_to_VGA(uint16_t x_coor, uint16_t y_coor, uint16_t character_w, uint16_t character_h, uint8_t color, const uint8_t *Pbitmap){
+TEXT_ERROR_CODES API_Write_Small_Character_to_VGA(uint16_t x_coor, uint16_t y_coor, uint16_t character_w, uint16_t character_h, uint8_t color, const uint8_t *Pbitmap){
 
+	VGA_INIT_ERROR_CODES error = 0;
 	for(uint16_t i = y_coor; i < (character_h + y_coor); i++){
 		for(uint16_t j = x_coor; j < (character_w + x_coor); j++){
 			if(*Pbitmap == 0xFF)
-				API_SetPixel(j, i, Background_Color);
+				error = API_SetPixel(j, i, Background_Color);
 			else if(*Pbitmap == 0x00)
-				API_SetPixel(j, i, color);
+				error = API_SetPixel(j, i, color);
 
 			Pbitmap++;
 		}
 	}
+
+	if(error != VGA_SETPIXEL_SUCCESS)
+		return ERROR_WRITE_CHARACTER;
+	return WRITE_CHARACTER_SUCCESS;
 }
+
+TEXT_ERROR_CODES API_Write_Big_Character_to_VGA(uint16_t x_coor, uint16_t y_coor, uint16_t character_w, uint16_t character_h, uint8_t color, const uint8_t *Pbitmap){
+
+	VGA_INIT_ERROR_CODES error = 0;
+	for(uint16_t i = y_coor; i < (character_h + y_coor); i += 2){
+		for(uint16_t j = x_coor; j < (character_w + x_coor); j+= 2){
+			if(*Pbitmap == 0xFF){
+				error = API_SetPixel(j    , i    , Background_Color);
+				error = API_SetPixel(j + 1, i    , Background_Color);
+				error = API_SetPixel(j    , i + 1, Background_Color);
+				error = API_SetPixel(j +1 , i + 1, Background_Color);
+			}
+			else if(*Pbitmap == 0x00){
+				error = API_SetPixel(j    , i    , color);
+				error = API_SetPixel(j + 1, i    , color);
+				error = API_SetPixel(j    , i + 1, color);
+				error = API_SetPixel(j +1 , i + 1, color);
+			}
+			Pbitmap++;
+		}
+	}
+
+	if(error != VGA_SETPIXEL_SUCCESS)
+		return ERROR_WRITE_CHARACTER;
+	return WRITE_CHARACTER_SUCCESS;
+}
+
--- Library/text.h
+++ Library/text.h
@@ -8,17 +8,25 @@
 #include "minecraft.h"
 #include "bitmap.h"
 
-#define MAX_CHARACTER_WIDTH 	7
-#define ASCII_OFFSET			32
+#define MAX_SMALL_CHARACTER_WIDTH 	7
+#define MAX_BIG_CHARACTER_WIDTH 	14
+#define ASCII_OFFSET				32
+
+#define FONTSIZE_SMALL				0
+#define FONTSIZE_BIG				1
 
 typedef enum{
-	DRAW_TEXT_SUCCESS			= 0,
+	DRAW_TEXT_SUCCESS				= 0,
 	ERROR_FONT_NOT_AVAILABLE,
-	ERROR_TEXT_TOO_LONG
+	ERROR_FONTSIZE_NOT_AVAILABLE,
+	ERROR_TEXT_TOO_LONG,
+	ERROR_WRITE_CHARACTER,
+	WRITE_CHARACTER_SUCCESS
 }TEXT_ERROR_CODES;
 
 
-TEXT_ERROR_CODES API_Draw_Text				(uint16_t x_coor, uint16_t y_coor, uint8_t color, char *text, char *fontname, uint8_t fontsize, uint8_t fontstyle);
-TEXT_ERROR_CODES API_Write_Character_to_VGA	(uint16_t x_coor, uint16_t y_coor, uint16_t character_w, uint16_t character_h, uint8_t color, const uint8_t *Pbitmap);
+TEXT_ERROR_CODES API_Draw_Text						(uint16_t x_coor, uint16_t y_coor, uint8_t color, char *text, char *fontname, uint8_t fontsize, uint8_t fontstyle);
+TEXT_ERROR_CODES API_Write_Small_Character_to_VGA	(uint16_t x_coor, uint16_t y_coor, uint16_t character_w, uint16_t character_h, uint8_t color, const uint8_t *Pbitmap);
+TEXT_ERROR_CODES API_Write_Big_Character_to_VGA		(uint16_t x_coor, uint16_t y_coor, uint16_t character_w, uint16_t character_h, uint8_t color, const uint8_t *Pbitmap);
 
 #endif //__TEXT_H
