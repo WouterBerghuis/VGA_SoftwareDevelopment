b46f9c416c75c5d64ed7434908671d2fd596bc5f
WouterBerghuis <berghuis@delmic.com>
2020-05-01 16:17:51 +0200

    Added doxygen documentation

--- Library/rectangle.c
+++ Library/rectangle.c
@@ -12,6 +12,31 @@
 
 #include "rectangle.h"
 
+/**
+  * @brief	This function is used for drawing a rectangle on the VGA screen.
+  *
+  * Drawing the rectangle on the screen consist of the following steps:
+  * 1. Check the received coordinates of the rectangle.
+  * 	 Does the rectangle fit on the screen or is the size too big
+  * 	 for the chosen position.
+  *	2. Write the rectangle pixel data to the VGA RAM
+  *	     This is done with the use of the API_Draw_Line function
+  *	3. Fill the inside of the rectangle if @param filled is equal to one
+  *		 This is done by setting the pixels within the rectangle corners
+  *		 to @param color.
+  *
+  * @param	x_coor This is the x-coordinate for the rectangle, (uint16_t)
+  * @param  y_coor This is the y-coordinate for the rectangle, (uint16_t)
+  * @param  width  This is the width of the rectangle, (uint16_t)
+  * @param  height This is the height of the rectangle, (uint16_t)
+  * @param  color  This is the color with which the rectangle is drawn, (uint8_t)
+  * @param  filled 0 = Not filled | 1 = Filled, if filled the inside of the
+  * 				rectangle will be filled with @param color, (uint8_t)
+  *
+  * @retval	RECTANGLE_ERROR_CODES
+  *
+  * @see RECTANGLE_ERROR_CODES
+  */
 RECTANGLE_ERROR_CODES API_Draw_Rectangle(uint16_t x_coor, uint16_t y_coor, uint16_t width, uint16_t height, uint8_t color,
 										 uint8_t filled){
 	RECTANGLE_ERROR_CODES error = 0;
@@ -32,22 +57,56 @@ RECTANGLE_ERROR_CODES API_Draw_Rectangle(uint16_t x_coor, uint16_t y_coor, uint1
 	return DRAW_RECTANGLE_SUCCESS;
 }
 
+/**
+  * @brief	This function is used to check if the received rectangle fits on the screen
+  *
+  * The check consist of checking each corner of the rectangle with the resolution of the VGA screen.
+  * If the rectangle does not fit on the screen the function returns an error code.
+  *
+  * @param	x_coor This is the x-coordinate for the rectangle, (uint16_t)
+  * @param  y_coor This is the y-coordinate for the rectangle, (uint16_t)
+  * @param  width  This is the width of the rectangle, (uint16_t)
+  * @param  height This is the height of the rectangle, (uint16_t)
+  *
+  * @retval	RECTANGLE_ERROR_CODES
+  *
+  * @see RECTANGLE_ERROR_CODES
+  */
 RECTANGLE_ERROR_CODES API_Check_Rectangle_Position(uint16_t x_coor, uint16_t y_coor, uint16_t width, uint16_t height){
 	// Check X coordinates
-	if(x_coor < 0)
+	if(x_coor < 0)							// Rectangle is placed too far to the left
 		return ERROR_POSITION_X_COOR;
-	if((x_coor + width) > VGA_DISPLAY_X)
+	if((x_coor + width) > VGA_DISPLAY_X)	// Rectangle is placed too far to the right
 		return ERROR_POSITION_X_COOR;
 
 	// Check Y coordinates
-	if(y_coor < 0)
+	if(y_coor < 0)							// Rectangle is placed too far top
 		return ERROR_POSITION_Y_COOR;
-	if((y_coor + height) > VGA_DISPLAY_Y)
+	if((y_coor + height) > VGA_DISPLAY_Y)	// Rectangle is placed too far bottom
 		return ERROR_POSITION_Y_COOR;
 
 	return CORRECT_RECTANGLE_POSITION;
 }
 
+/**
+  * @brief	This function writes the rectangle pixel data to the VGA RAM
+  *
+  * To write the pixel data to the VGA RAM the function is using the API_Draw_Line function.
+  * Using this function the four different lines of the rectangle can be drawn.
+  *
+  * @attention A weird bug which is solved with a hack. The right vertical line misses one pixel. To fix this
+  * the line is extended with one pixel.
+  *
+  * @param	x_coor This is the x-coordinate for the rectangle, (uint16_t)
+  * @param  y_coor This is the y-coordinate for the rectangle, (uint16_t)
+  * @param  width  This is the width of the rectangle, (uint16_t)
+  * @param  height This is the height of the rectangle, (uint16_t)
+  * @param  color  This is the color with which the rectangle is drawn, (uint8_t)
+  *
+  * @retval	RECTANGLE_ERROR_CODES
+  *
+  * @see RECTANGLE_ERROR_CODES
+  */
 RECTANGLE_ERROR_CODES API_Write_Rectangle_to_VGA(uint16_t x_coor, uint16_t y_coor, uint16_t width, uint16_t height, uint8_t color){
 	LINE_ERROR_CODES error;
 
@@ -75,6 +134,23 @@ RECTANGLE_ERROR_CODES API_Write_Rectangle_to_VGA(uint16_t x_coor, uint16_t y_coo
 	return DRAW_RECTANGLE_SUCCESS;
 }
 
+/**
+  * @brief	This function fills the inside of the rectangle with one color
+  *
+  * Filling the rectangle is done by looping over the individual pixels within the rectangle area.
+  * Each individual pixel is set to the @param color.
+  *
+  *
+  * @param	x_coor This is the x-coordinate for the rectangle, (uint16_t)
+  * @param  y_coor This is the y-coordinate for the rectangle, (uint16_t)
+  * @param  width  This is the width of the rectangle, (uint16_t)
+  * @param  height This is the height of the rectangle, (uint16_t)
+  * @param  color  This is the color with which the rectangle is drawn, (uint8_t)
+  *
+  * @retval	RECTANGLE_ERROR_CODES
+  *
+  * @see RECTANGLE_ERROR_CODES
+  */
 RECTANGLE_ERROR_CODES API_Fill_Rectangle(uint16_t x_coor, uint16_t y_coor, uint16_t width, uint16_t height, uint8_t color){
 	VGA_INIT_ERROR_CODES error = 0;
 	for(uint16_t i = y_coor; i < (height + y_coor); i++){
@@ -86,4 +162,3 @@ RECTANGLE_ERROR_CODES API_Fill_Rectangle(uint16_t x_coor, uint16_t y_coor, uint1
 	}
 	return FILL_RECTANGLE_SUCCESS;
 }
-
--- Library/rectangle.h
+++ Library/rectangle.h
@@ -12,23 +12,33 @@
 #ifndef __RECTANGLE_H
 #define __RECTANGLE_H
 
+/* Includes ------------------------------------------------------------------*/
 #include <stdio.h>
 #include "line.h"
 
+/**
+  * @brief Defines used for in the different rectangle drawing functions
+  */
 #define RECTANGLE_WEIGHT 			1
-#define RECTANGLE_LINE_OFFSET_BUG 	1 /**< While drawing the most right line of the rectangle a bug shows up, to fix this the line should be one pixel longr */
+#define RECTANGLE_LINE_OFFSET_BUG 	1 	/**< While drawing the most right line of the rectangle a bug shows up, to fix this the line should be one pixel longer */
 
+/**
+  * @brief Error codes for the rectangle drawing functions
+  */
 typedef enum{
-	DRAW_RECTANGLE_SUCCESS = 0,
-	DRAW_RECTANGLE_FAILED,
-	ERROR_POSITION_X_COOR,
-	ERROR_POSITION_Y_COOR,
-	CORRECT_RECTANGLE_POSITION,
-	ERROR_LINE_PLACEMENT,
-	ERROR_FILL_RECTANGLE,
-	FILL_RECTANGLE_SUCCESS
+	DRAW_RECTANGLE_SUCCESS = 0, 		/**< No error occurred during drawing of rectangle 					*/
+	DRAW_RECTANGLE_FAILED,				/**< Error occurred during drawing of rectangle 					*/
+	ERROR_POSITION_X_COOR,				/**< Error horizontal line position, placed to far left or right 	*/
+	ERROR_POSITION_Y_COOR,				/**< Error vertical line position, placed to far top or bottom 		*/
+	CORRECT_RECTANGLE_POSITION,			/**< Position off the rectangle is correct, fits on the screen		*/
+	ERROR_LINE_PLACEMENT,				/**< Error occurred during placing individual line					*/
+	ERROR_FILL_RECTANGLE,				/**< Error occurred during filling the rectangle					*/
+	FILL_RECTANGLE_SUCCESS				/**< No error occurred during placing individual line					*/
 }RECTANGLE_ERROR_CODES;
 
+/**
+  * @brief Draw rectangle functions
+  */
 RECTANGLE_ERROR_CODES API_Draw_Rectangle			(uint16_t x_coor, uint16_t y_coor, uint16_t width, uint16_t height, uint8_t color, uint8_t filled);
 RECTANGLE_ERROR_CODES API_Check_Rectangle_Position	(uint16_t x_coor, uint16_t y_coor, uint16_t width, uint16_t height);
 RECTANGLE_ERROR_CODES API_Write_Rectangle_to_VGA	(uint16_t x_coor, uint16_t y_coor, uint16_t width, uint16_t height, uint8_t color);
