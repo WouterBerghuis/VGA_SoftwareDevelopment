25b562f130015881da37b5275d15e2c62735fc3a
bestshield <33286246+bestshield@users.noreply.github.com>
2020-05-05 16:39:04 +0200

    Deleted Colorstringtoint and fontstylestringtoint

--- Library/VGA_core.c
+++ Library/VGA_core.c
@@ -158,144 +158,77 @@ VGA_INIT_ERROR_CODES API_SetPixel(uint16_t xp, uint16_t yp, uint8_t color)
 VGA_INIT_ERROR_CODES Check_Color(char *Color, uint8_t *colorValue){
 	VGA_INIT_ERROR_CODES error;
 	if (strcmp(Color, "zwart") == 0)
-		error = Color_String_to_Int(Color, colorValue);
+		*colorValue = VGA_COL_BLACK;
 
 	else if (strcmp(Color, "blauw") == 0)
-		error = Color_String_to_Int(Color, colorValue);
+		*colorValue = VGA_COL_BLUE;
 
 	else if (strcmp(Color, "lichtblauw") == 0)
-		error = Color_String_to_Int(Color, colorValue);
+		*colorValue = VGA_COL_LIGHTBLUE;
 
 	else if (strcmp(Color, "groen") == 0)
-		error = Color_String_to_Int(Color, colorValue);
+		*colorValue = VGA_COL_GREEN;
 
 	else if (strcmp(Color, "lichtgroen") == 0)
-		error = Color_String_to_Int(Color, colorValue);
+		*colorValue = VGA_COL_LIGHTGREEN;
 
 	else if (strcmp(Color, "cyaan") == 0)
-		error = Color_String_to_Int(Color, colorValue);
+		*colorValue = VGA_COL_CYAN;
 
 	else if (strcmp(Color, "lichtcyaan") == 0)
-		error = Color_String_to_Int(Color, colorValue);
+		*colorValue = VGA_COL_LIGHTCYAN;
 
 	else if (strcmp(Color, "rood") == 0)
-		error = Color_String_to_Int(Color, colorValue);
+		*colorValue = VGA_COL_RED;
 
 	else if (strcmp(Color, "lichtrood") == 0)
-		error = Color_String_to_Int(Color, colorValue);
+		*colorValue = VGA_COL_LIGHTRED;
 
 	else if (strcmp(Color, "magenta") == 0)
-		error = Color_String_to_Int(Color, colorValue);
+		*colorValue = VGA_COL_MAGENTA;
 
 	else if (strcmp(Color, "lichtmagenta") == 0)
-		error = Color_String_to_Int(Color, colorValue);
+		*colorValue = VGA_COL_LIGHTMAGENTA;
 
 	else if (strcmp(Color, "bruin") == 0)
-		error = Color_String_to_Int(Color, colorValue);
+		*colorValue = VGA_COL_BROWN;
 
 	else if (strcmp(Color, "geel") == 0)
-		error = Color_String_to_Int(Color, colorValue);
+		*colorValue = VGA_COL_YELLOW;
 
 	else if (strcmp(Color, "grijs") == 0)
-		error = Color_String_to_Int(Color, colorValue);
+		*colorValue = VGA_COL_GREY;
 
 	else if (strcmp(Color, "wit") == 0){
-		error = Color_String_to_Int(Color, colorValue);
+		*colorValue = VGA_COL_WHITE;
 	}
 	else
 		return COLOR_NOT_FOUND;
-	return error;
+	return COLOR_FOUND;
 }
 
 /**
-  * @brief	This function returns the decimal value of the colour sent to the function
+  * @brief	This function checks the fontstyle of the string
   *
-  * @param	Colorstring Pointer to the string that contains the colour, (char *)
-  * @param	colorValue Pointer to the variable that will hold the uint8_t value of the color, (int *)
+  * @param	Fontstylestring Pointer to the string that contains the fontstyle, (char *)
+  * @param	fontstyleValue Pointer to the variable that will hold the uint8_t value of the color, (int *)
   * @retval	VGA_INIT_ERROR_CODES
   *
   * @see VGA_INIT_ERROR_CODES
   */
-VGA_INIT_ERROR_CODES Color_String_to_Int(char *Colorstring, uint8_t *colorValue)
-{
-
-	if (strcmp(Colorstring, "zwart") == 0)
-		*colorValue = VGA_COL_BLACK ;
-
-	else if (strcmp(Colorstring, "blauw") == 0)
-		*colorValue = VGA_COL_BLUE ;
-
-	else if (strcmp(Colorstring, "lichtblauw") == 0)
-		*colorValue = VGA_COL_LIGHTBLUE;
-
-	else if (strcmp(Colorstring, "groen") == 0)
-		*colorValue = VGA_COL_GREEN ;
-
-	else if (strcmp(Colorstring, "lichtgroen") == 0)
-		*colorValue = VGA_COL_LIGHTGREEN;
-
-	else if (strcmp(Colorstring, "cyaan") == 0)
-		*colorValue = VGA_COL_CYAN ;
-
-	else if (strcmp(Colorstring, "lichtcyaan") == 0)
-		*colorValue = VGA_COL_LIGHTCYAN;
-
-	else if (strcmp(Colorstring, "rood") == 0)
-		*colorValue = VGA_COL_RED;
-
-	else if (strcmp(Colorstring, "lichtrood") == 0)
-		*colorValue = VGA_COL_LIGHTRED;
-
-	else if (strcmp(Colorstring, "magenta") == 0)
-		*colorValue = VGA_COL_MAGENTA;
-
-	else if (strcmp(Colorstring, "lichtmagenta") == 0)
-		*colorValue = VGA_COL_LIGHTMAGENTA;
-
-	else if (strcmp(Colorstring, "bruin") == 0)
-		*colorValue = VGA_COL_BROWN;
-
-	else if (strcmp(Colorstring, "geel") == 0)
-		*colorValue = VGA_COL_YELLOW;
-
-	else if (strcmp(Colorstring, "grijs") == 0)
-		*colorValue = VGA_COL_GREY;
-
-	else if (strcmp(Colorstring, "wit") == 0)
-		*colorValue = VGA_COL_WHITE;
-
-	else
-		return COLOR_TO_STRING_FAILED;
-
-	return COLOR_TO_STRING_SUCCES;
-}
-
 VGA_INIT_ERROR_CODES Check_Fontstyle(char *Fontstylestring, uint8_t *fontstyleValue){
-	VGA_INIT_ERROR_CODES error;
-		if (strcmp(Fontstylestring, "normaal") == 0)
-			error = Fontstyle_String_to_Int(Fontstylestring, fontstyleValue);
-
-		else if (strcmp(Fontstylestring, "vet") == 0)
-			error = Fontstyle_String_to_Int(Fontstylestring, fontstyleValue);
-
-		else if (strcmp(Fontstylestring, "cursief") == 0)
-			error = Fontstyle_String_to_Int(Fontstylestring, fontstyleValue);
-
-		else
-			return FONTSTYLE_NOT_FOUND;
-		return error;
-}
-
-VGA_INIT_ERROR_CODES Fontstyle_String_to_Int(char *Fontstylestring, uint8_t *fontstyleValue){
 		if (strcmp(Fontstylestring, "normaal") == 0)
 			*fontstyleValue = FONTSTYLE_NORMAL;
+
+		else if (strcmp(Fontstylestring, "vet") == 0)
+			*fontstyleValue = FONTSTYLE_BOLD;
 
 		else if (strcmp(Fontstylestring, "cursief") == 0)
 			*fontstyleValue = FONTSTYLE_ITALIC;
 
-		else if (strcmp(Fontstylestring, "vet") == 0)
-			*fontstyleValue = FONTSTYLE_BOLD;
 		else
-			return FONTSTYLE_TO_INT_FAILED;
-		return FONTSTYLE_TO_INT_SUCCESS;
+			return FONTSTYLE_NOT_FOUND;
+		return FONTSTYLE_FOUND;
 }
+
+
